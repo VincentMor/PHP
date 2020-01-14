7bb152820dd75d216169fcdf3286aea427488b2f
Vincent Morel <lP@invalid>
2020-01-14 16:05:20 +0100

    test

--- initGit.sh
+++
@@ -1,6 +0,0 @@
-git config --global user.name "Vincent Morel"
-git config --global user.email "lP@invalid"
-git config --global color.ui true
-git config --global core.editor nano
-git config --global merge.ff false
-

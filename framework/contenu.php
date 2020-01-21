684d6ab707bbb7e22aaebc48a4534a348ac326f6
Vincent Morel <lP@invalid>
2020-01-21 13:35:39 +0100

    Ajout du framework

---
+++ framework/contenu.php
@@ -0,0 +1,12 @@
+<?php
+$page="accueil";
+// ajouter un tableau avec le nom des pages autorisée et verifier si la page demande en fait partie
+
+if (isset($_GET["page"])) {
+	$page=$_GET["page"];
+}
+
+include $page.".php";
+
+
+?>
---
+++ framework/corps.php
@@ -0,0 +1,19 @@
+
+<?php
+echo "<div id='banniere'>";
+	include ("banniere.php");
+echo "</div>";
+
+echo "<div id='centre'>";
+	echo "<div id='menu'>";
+		include ("menu.php");
+	echo "</div>";
+
+	echo "<div id='contenu'>";
+		include ("contenu.php");
+	echo "</div>";
+	
+
+echo "</div>";
+
+?>

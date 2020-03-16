ce87a8872c0df2f06edff9acd9f42a0703ecd485
Vincent Morel <Vincent@example.com>
2020-03-16 14:02:55 +0100

    V debut

--- framework/banniere.php
+++ framework/banniere.php
@@ -1,8 +1,23 @@
-<?php 
 
+<?php
+include("../Controleur/connexionBD.php");
+if(isset($_SESSION["login"])){
+	if($_SESSION["role"]=="admin"){
+		include("../Modele/gestionCircuit_modele.php");
+		echo "<h2>Circuit</h2>";
+	}else{
+		echo "<h2> Documents à Traiter</h2>";
+	}
+}
 ?>
-<h2> Documents à Traiter</h2>
 <ul>
+<?php 
+while ($circuit = $requete_listeCircuit -> fetch()){
+	echo "<li>";
+	echo "<a href=\"index.php?page=gestionCircuitControleur&id=".$circuit[].">".$circuit["nom"]."</a>";
+	echo "</li>";
+}
+?>
 	<li></li>
 	<li></li>
 	<li></li>
